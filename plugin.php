359a406a531d64121a026b2126232cb94315fc90
ZQE <dev@zqe.io>
2022-10-19 13:47:33 +0600

    update

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Wp Term Meta
- * Plugin URI: https://github.com/zqelab/wp-term-meta
- * Description: WordPress Term Meta
+ * Plugin URI: https://github.com/zqelab/wp-settings-api
+ * Description: Wp Term Meta
  * Author: ZQE
  * Author URI: https://zqe.io
  * Version: 1.0
